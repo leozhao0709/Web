4d6280cf45b6055868a54492ffe9582a54db6a7a
Lei Zhao <zhao434@usc.edu>
2018-09-21 20:00:10 -0700

    2018-09-21 20:00:06 auto check in

--- udemy_angular/ng-rxjs/src/app/app.component.ts
+++ udemy_angular/ng-rxjs/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Subject} from 'rxjs';
+import {filter, map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-rxjs-test-again';
+
+  ngOnInit(): void {
+    const subject = new Subject();
+
+    const observable = subject.asObservable()
+      .pipe(
+        map((val: number) => val * 3),
+        filter(val => val % 2 === 0)
+      )
+      .subscribe(console.log)
+    ;
+
+
+    subject.next(1);
+    subject.next(2);
+    subject.next(3);
+    subject.next(4);
+  }
+
 }
