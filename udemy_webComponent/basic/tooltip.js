8301975c13ba965fe69ac0fdfee7be04ad5deb05
lzhao <zhao434@usc.edu>
2019-03-14 18:43:15 -0700

    test web component

--- udemy_webComponent/basic/tooltip.js
+++ udemy_webComponent/basic/tooltip.js
@@ -1,8 +1,31 @@
 class Tooltip extends HTMLElement {
   constructor() {
     super();
-    // eslint-disable-next-line no-console
-    console.log('This is my tool tip');
+    this._tooltipText = 'Default tooltip text';
+    this._tooltipContainer;
+  }
+
+  // call all the dom staff here
+  connectedCallback() {
+    if (this.getAttribute('text')) {
+      this._tooltipText = this.getAttribute('text');
+    }
+    this.addEventListener('mouseenter', () => {
+      this._tooltipContainer = document.createElement('div');
+      this._tooltipContainer.textContent = this._tooltipText;
+      this.appendChild(this._tooltipContainer);
+
+      this.style.position = 'relative';
+      this._tooltipContainer.style.position = 'absolute';
+      this._tooltipContainer.style.zIndex = 10;
+      this._tooltipContainer.style.backgroundColor = 'yellow';
+      this._tooltipContainer.style.left = '50%';
+      this.style.backgroundColor = 'orange';
+    });
+
+    this.addEventListener('mouseleave', () => {
+      this.removeChild(this._tooltipContainer);
+    });
   }
 }
 
