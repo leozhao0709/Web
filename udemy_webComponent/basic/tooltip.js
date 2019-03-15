6808b1f5b68c847d3d31c52139a247794a712060
Lei Zhao <zhao434@usc.edu>
2019-03-15 06:40:48 -0700

    2019-03-15 06:40:46 auto check in

--- udemy_webComponent/basic/tooltip.js
+++ udemy_webComponent/basic/tooltip.js
@@ -3,28 +3,41 @@ class Tooltip extends HTMLElement {
     super();
     this._tooltipText = 'Default tooltip text';
     this._tooltipContainer;
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.innerHTML = `
+      <style>
+        div {
+          background-color: black;
+          color: white;
+          position: absolute;
+          z-index: 10;
+          left: 50%;
+          transform: translateX(-50%);
+        }
+        :host {
+          border: 1px solid #000;
+        }
+      </style>
+      <slot>Some default</slot>
+    `;
   }
 
   // call all the dom staff here
   connectedCallback() {
     if (this.getAttribute('text')) {
-      this._tooltipText = this.getAttribute('text');
+      this._tooltipText = this.getAttribute('text'); // you can define your component attribute and get it like this
     }
+
+    this.style.backgroundColor = 'orange';
+    this.style.position = 'relative';
     this.addEventListener('mouseenter', () => {
       this._tooltipContainer = document.createElement('div');
       this._tooltipContainer.textContent = this._tooltipText;
-      this.appendChild(this._tooltipContainer);
-
-      this.style.position = 'relative';
-      this._tooltipContainer.style.position = 'absolute';
-      this._tooltipContainer.style.zIndex = 10;
-      this._tooltipContainer.style.backgroundColor = 'yellow';
-      this._tooltipContainer.style.left = '50%';
-      this.style.backgroundColor = 'orange';
+      this.shadowRoot.appendChild(this._tooltipContainer);
     });
 
     this.addEventListener('mouseleave', () => {
-      this.removeChild(this._tooltipContainer);
+      this.shadowRoot.removeChild(this._tooltipContainer);
     });
   }
 }
