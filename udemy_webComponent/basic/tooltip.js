5d730ac21082434d38010be950d8715d6a9cf5d6
Lei Zhao <zhao434@usc.edu>
2019-03-18 06:37:48 -0700

    2019-03-18 06:37:46 auto check in

--- udemy_webComponent/basic/tooltip.js
+++ udemy_webComponent/basic/tooltip.js
@@ -3,6 +3,7 @@ class Tooltip extends HTMLElement {
     super();
     this._tooltipText = 'Default tooltip text';
     this._tooltipContainer;
+    this._tooltipIcon;
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.innerHTML = `
       <style>
@@ -13,6 +14,8 @@ class Tooltip extends HTMLElement {
           z-index: 10;
           left: 50%;
           transform: translateX(-50%);
+          border-radius: 3px;
+          box-shadow: 1px 1px 6px rgba(0,0,0,0.26);
         }
         :host {
           border: 1px solid #000;
@@ -23,8 +26,18 @@ class Tooltip extends HTMLElement {
           background-color: yellow;
         }
 
+        .icon {
+          background: black;
+          color: white;
+          padding: 0.15rem 0.5rem;
+          text-align: center;
+          border-radius: 50%;
+          position: relative;
+        }
+
       </style>
       <slot>Some default</slot>
+      <span class="icon">?</span>
     `;
   }
 
@@ -33,16 +46,39 @@ class Tooltip extends HTMLElement {
     if (this.getAttribute('text')) {
       this._tooltipText = this.getAttribute('text'); // you can define your component attribute and get it like this
     }
+    this._tooltipIcon = this.shadowRoot.querySelector('span');
+    this._tooltipIcon.addEventListener('mouseenter', () => this._showTooltip());
 
-    this.addEventListener('mouseenter', () => {
-      this._tooltipContainer = document.createElement('div');
-      this._tooltipContainer.textContent = this._tooltipText;
-      this.shadowRoot.appendChild(this._tooltipContainer);
-    });
+    this._tooltipIcon.addEventListener('mouseleave', () => this._hideTooltip());
+  }
 
-    this.addEventListener('mouseleave', () => {
-      this.shadowRoot.removeChild(this._tooltipContainer);
-    });
+  _showTooltip() {
+    this._tooltipContainer = document.createElement('div');
+    this._tooltipContainer.textContent = this._tooltipText;
+    this._tooltipIcon.appendChild(this._tooltipContainer);
+  }
+
+  _hideTooltip() {
+    this._tooltipIcon.removeChild(this._tooltipContainer);
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue === newValue) {
+      return;
+    }
+
+    if (name === 'text') {
+      this._tooltipText = newValue;
+    }
+  }
+
+  static get observedAttributes() {
+    return ['text', 'class'];
+  }
+
+  disconnectedCallback() {
+    this._tooltipIcon.removeEventListener('mouseenter');
+    this._tooltipIcon.removeEventListener('mouseleave');
   }
 }
 
