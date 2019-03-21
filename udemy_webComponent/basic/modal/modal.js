dd16362bfa1059e92fa6a77f0f4ffa6bcff2d0af
Lei Zhao <zhao434@usc.edu>
2019-03-21 06:20:39 -0700

    2019-03-21 06:20:37 auto check in

--- udemy_webComponent/basic/modal/modal.js
+++ udemy_webComponent/basic/modal/modal.js
@@ -2,6 +2,7 @@ class Modal extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
+    this.isOpen;
     this.shadowRoot.innerHTML = `
         <style>
             #backdrop {
@@ -12,6 +13,8 @@ class Modal extends HTMLElement {
                 height: 100vh;
                 background-color: rgba(0, 0, 0, 0.25);
                 z-index: 10;
+                opacity: 0;
+                pointer-events: none;
             }
             #modal {
                 position: fixed;
@@ -19,15 +22,88 @@ class Modal extends HTMLElement {
                 left: 25%;
                 top: 15vh;
                 width: 50%;
-                height: 30rem;
                 background-color: white;
                 border-radius: 3px;
                 box-shadow: 0 2px 8px rgba(0, 0, 0, 0.5);
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
+                opacity: 0;
+                pointer-events: none;
+            }
+
+            :host([opened]) #modal,
+            :host([opened]) #backdrop {
+                opacity: 1;
+                pointer-events: all;
+            }
+
+            header {
+                padding: 1rem;
+            }
+
+            ::slotted(h1) {
+                font-size: 1.25rem;
+            }
+
+            #main {
+                padding: 1rem;
+            }
+
+            #actions {
+                border-top: 1px solid #ccc;
+                padding: 1rem;
+                display: flex;
+                justify-content: flex-end;
+            }
+
+            #actions button {
+                margin: 0 0.25rem;
             }
         </style>
         <div id='backdrop'></div>
-        <div id='modal'></div>
+        <div id='modal'>
+            <header>
+                <slot name='title'><h1>Please Confirm</h1></slot>
+            </header>
+            <section id="main">
+                <slot></slot>
+            </section>
+            <section id="actions">
+                <button>Cancel</button>
+                <button>Okay</button>
+            </section>
+        </div>
     `;
+  }
+
+  connectedCallback() {
+    const backdrop = this.shadowRoot.querySelector('#backdrop');
+    backdrop.addEventListener('click', () => {
+      this.close();
+    });
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue === newValue) {
+      return;
+    }
+
+    if (name === 'opened') {
+      this.isOpen = !this.isOpen;
+    }
+  }
+
+  static get observedAttributes() {
+    return ['opened'];
+  }
+
+  open() {
+    this.setAttribute('opened', '');
+  }
+
+  close() {
+    this.removeAttribute('opened');
   }
 }
 
