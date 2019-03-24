c2bcf105e8388b669411fe4b3c1c508d82957047
Lei Zhao <zhao434@usc.edu>
2019-03-24 07:20:02 -0700

    2019-03-24 07:20:00 auto check in

--- udemy_webComponent/stencil/web-components-stencil/src/components/side-draw/side-draw.tsx
+++ udemy_webComponent/stencil/web-components-stencil/src/components/side-draw/side-draw.tsx
@@ -10,15 +10,31 @@ export class SideDrawer {
   @Prop({ reflectToAttr: true, mutable: true }) open = false;
 
   render() {
+    let mainContent = <slot />;
+    mainContent = (
+      <div id="contact-information">
+        <h2>Contact Information</h2>
+        <p>You can reach us via phone or email.</p>
+        <ul>
+          <li>Phone: (xxx)-xxx-xxx</li>
+          <li>
+            E-Mail: <a href="mailto: something@somewhere.com">something@somewhere.com</a>
+          </li>
+        </ul>
+      </div>
+    );
+
     return (
       <aside>
         <header>
           <h1>{this.title}</h1>
           <button onClick={() => (this.open = false)}>X</button>
         </header>
-        <main>
-          <slot />
-        </main>
+        <section id="tab">
+          <button class="active">Navigation</button>
+          <button>Contact</button>
+        </section>
+        <main>{mainContent}</main>
       </aside>
     );
   }
