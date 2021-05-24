02ccf3517c9fc550ae66a2ff6bd73cec4e8b6a84
Lei Zhao <lei.zhao@coupa.com>
2021-05-24 09:05:52 -0700

    add by auto checkIn

---
+++ vue_learn/vue3-basic/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <h1>{{count}}</h1>
+  <button @click="increase">👍+1</button>
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+export default {
+  name: "App",
+  setup() {
+    const count = ref(0);
+    const increase = () => {
+      count.value++;
+    };
+    return {
+      count,
+      increase
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
